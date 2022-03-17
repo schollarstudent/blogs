c1de02d4117126d71dd6d18a61b2d7ab01dd434a
fmul <fmul@fresnou.org>
2022-03-17 08:10:10 -0700

    inital commit

--- models/reply.js
+++ models/reply.js
@@ -13,7 +13,7 @@ module.exports = (sequelize, DataTypes) => {
         static associate(models) {
             // define association here
         }
-    };
+    }
     Reply.init({
         author_name: DataTypes.STRING,
         body: DataTypes.STRING,
